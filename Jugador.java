4df58562893c839b4b1a85a81b655f8c7dede600
JorgeNietoT <58139267+JorgeNietoT@users.noreply.github.com>
2019-11-29 08:48:43 +0100

    Añado la clase jugador
    Esta es la clase jugador

---
+++ Jugador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistaprincipalrosco;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Jugador {
+  
+    String nombre;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //incluir metodos
+    
+}
