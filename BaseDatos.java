6fb98e154ed81e6cd7a5d4438b8ee6591793eee7
DenisMNF <58033521+DenisMNF@users.noreply.github.com>
2019-11-29 09:27:09 +0100

    Add files via upload

--- BaseDatos.java
+++ BaseDatos.java
@@ -7,10 +7,13 @@ Funciones:
 package proyectorosco;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
@@ -22,6 +25,7 @@ public class BaseDatos {
 /************************************PARAMETROS********************************************/
     private final String ESPAÑOL = "spanish.txt";
     private final String INGLES = "english.txt";
+    private final String path_puntuaciones = "historico_puntuaciones.txt";
     //
     private final int NUM_LETRAS = 26;
     //
@@ -30,19 +34,22 @@ public class BaseDatos {
     
 /*************************************ATRIBUTOS********************************************/
     private ArrayList<String> preguntas;
-    private File f;
+    private File idioma_file;
+    private File puntuaciones_file = new File(path_puntuaciones);
+    
 /************************************REFERENCIAS*******************************************/
 /************************************CONSTRUCTOR*******************************************/
     public BaseDatos() throws IOException{
-        f = new File(ESPAÑOL);
-        cargarPreguntas(f);
+        idioma_file = new File(ESPAÑOL);
+        cargarPreguntas(idioma_file);
         mostrarDatos();
     }
 /***********************************METODOS CLASE******************************************/
+    
     private void cargarPreguntas(File f) throws FileNotFoundException, IOException{
         //
         preguntas = new ArrayList<String>();
-        BufferedReader lector = new BufferedReader( new FileReader(f));
+        BufferedReader lector = new BufferedReader( new FileReader(idioma_file));
         //
         String linea = lector.readLine();
         while(linea != null){
@@ -51,9 +58,33 @@ public class BaseDatos {
             linea = lector.readLine();
         }
         
+        lector.close();      
+    }
+    
+    public void registrarPuntuacionEnHistorico(String puntuacion) throws IOException{
+        
+        if(!puntuaciones_file.exists()){
+            //
+            puntuaciones_file.createNewFile();
+        }
+        PrintWriter escritor = new PrintWriter ( new BufferedWriter ( new FileWriter(puntuaciones_file,true)));
+        
+        escritor.println(puntuacion);
+        escritor.close();
+    }
+    
+    public ArrayList<String> cargarPuntuacionesDeHistorico() throws FileNotFoundException, IOException{
+        BufferedReader lector = new BufferedReader( new FileReader(puntuaciones_file));
+        
+        ArrayList<String> datosRecogidos = new ArrayList<String>();
+        
+        String linea = lector.readLine();
+        while( linea != null){
+            datosRecogidos.add(linea);
+            linea = lector.readLine();
+        }
         lector.close();
-        
-        
+        return datosRecogidos;
     }
     
     private void mostrarDatos(){
@@ -62,9 +93,13 @@ public class BaseDatos {
         }
     }
     
- 
 
 /*************************************INTERFAZ*********************************************/
+    public void limpiarHistoricoPuntuaciones(){
+        if(puntuaciones_file.exists()){
+            puntuaciones_file.delete();
+        }
+    }
    
 /***********************************GETTER SETTERS*****************************************/
      public int getNUM_LETRAS() {
